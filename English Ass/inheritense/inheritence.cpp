c0b67e32584f7026931c4805f7d945b8318212bd
Najam Ali Abbas <90936436+Ctoic@users.noreply.github.com>
2022-02-20 10:17:21 +0000

    Add files via upload

---
+++ English Ass/inheritense/inheritence.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class inheritence
+{
+private:
+    /* data */
+public:
+    inheritence(/* args */);
+    ~inheritence();
+};
+
+inheritence::inheritence(/* args */)
+{
+}
+
+inheritence::~inheritence()
+{
+}
+
