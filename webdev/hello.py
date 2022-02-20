bfd826f6438d45aa052ed80d9787758cc46cf929
Najam Ali Abbas <90936436+Ctoic@users.noreply.github.com>
2022-02-20 08:20:15 +0000

    Added new files

---
+++ webdev/hello.py
@@ -0,0 +1,3 @@
+for i in range(8):
+	for j in range(10);
+		print(j*i)
---
+++ webdev/labtask6.cpp
@@ -0,0 +1,12 @@
+today we are going to write our first hello world programm
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	cout<<"Hello world";
+	cout<<"HEKO world"<<endl;
+
+	return 0;
+}
